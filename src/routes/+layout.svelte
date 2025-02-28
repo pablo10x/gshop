79a81c7ac18aff3235bb18600fea45c850e7e408
pab <pabapple@icloud.com>
2025-02-28 19:51:21 +0100

    working on navbars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	import Navbar from '../lib/components/Navbar.svelte';
 	import { page } from '$app/stores';
 </script>
-<div class="min-h-screen bg-gradient-to-b from-slate-500 to-slate-50 dark:bg-gray-900 ">
+<div class="min-h-screen bg-gradient-to-b bg-slate-200 dark:bg-gray-900 ">
 	<Navbar />
 	<slot></slot>
 	</div>
